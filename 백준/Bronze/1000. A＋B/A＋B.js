c3a1e797aa3e98cb6fe27cb965259f78f85c0dbd
MaSeungHyun <66582559+MaSeungHyun@users.noreply.github.com>
2025-08-25 01:07:42 +0900

    [Bronze V] Title: A+B, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+function solution() {
+  const a = parseInt(input[0]);
+  const b = parseInt(input[1]);
+
+  console.log(a + b);
+}
+
+solution();
