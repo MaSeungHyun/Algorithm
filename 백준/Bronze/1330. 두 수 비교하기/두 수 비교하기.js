3a78c3b10be0948340272fc6a1e9d58c109a278c
MaSeungHyun <66582559+MaSeungHyun@users.noreply.github.com>
2025-08-25 01:20:44 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -5,13 +5,11 @@ function solution() {
   const a = parseInt(input[0]);
   const b = parseInt(input[1]);
 
-  const diff = a - b;
-  
-  if (diff < 0) {
+  if (a-b < 0) {
     console.log("<");
-  } else if (diff > 0) {
+  } else if (a-b > 0) {
     console.log(">");
-  } else if (diff === 0) {
+  } else if (a-b === 0) {
     console.log("==");
   } else {
     throw new Error("Invalid input values");
