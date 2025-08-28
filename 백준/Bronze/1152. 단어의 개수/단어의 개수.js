8d098385dc5734f6f8ede771598ee6054089185e
MaSeungHyun <66582559+MaSeungHyun@users.noreply.github.com>
2025-08-28 10:11:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 148 ms, Memory: 28312 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,20 +1,15 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(file, "utf8").toString().split("\n")
+const input = fs.readFileSync(file, "utf8").toString().trim();
 
 function solution() {
-    const array = input[0].split(" ");
-    let initCount = 0;
-
-    array.forEach((word, index) =>{
-        if(index === 0 || index === array.length - 1) {
-            if(word.trim().length === 0) {
-                initCount--;
-            }
-        }
-    })
-
-    console.log(array.length + initCount);
+    console.log(input ? input.split(/\s+/).length : 0);
 }
 
 solution();
+
+
+
+
+
+
